e491475635fcc945a0f9058b3e9958a7aa3bce55
Turan Gozukara <117595686+turangozukara@users.noreply.github.com>
2024-01-13 11:40:10 +0300

    Update app.py

--- img-for-web-server/app.py
+++ img-for-web-server/app.py
@@ -78,7 +78,6 @@ def delete_person(name):
     cursor.execute(delete)
     return f'Phone record of {name.strip().title()} is deleted from the phonebook successfully'
 
-# and assign to the static route of ('add')
 @app.route('/add', methods=['GET', 'POST'])
 def add_record():
     if request.method == 'POST':
